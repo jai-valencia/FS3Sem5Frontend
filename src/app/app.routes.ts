e14958d339207da9bd1c617b98857b3c0eb4d6b1
jai-valencia <jai.valencia@duocuc.cl>
2025-11-23 21:54:19 -0300

    Crea seguridad y rutas, login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { LaboratorioComponent } from './pages/laboratorio/laboratorio.component';
+import { ExamenesComponent } from './pages/examenes/examenes.component';
+import { AuthGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  
+  { path: 'usuarios', component: UsuariosComponent, canActivate: [AuthGuard] },
+  { path: 'laboratorio', component: LaboratorioComponent, canActivate: [AuthGuard] },
+  { path: 'examenes', component: ExamenesComponent, canActivate: [AuthGuard] },
+
+  { path: '', redirectTo: '/usuarios', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.scss',
+  templateUrl: './login.component.html'
 })
-export class Login {
+export class LoginComponent {
 
+  username = '';
+  password = '';
+  error = false;
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  onLogin() {
+    const ok = this.auth.login(this.username, this.password);
+
+    if (!ok) {
+      this.error = true;
+      return;
+    }
+
+    this.router.navigate(['/usuarios']);
+  }
 }
