017a4b347bc4a22b4796a676be6a9b6e11559a6c
jai-valencia <jai.valencia@duocuc.cl>
2025-11-23 21:06:07 -0300

    Se crean los archivos y repositorios a utilizar en el frontend

---
+++ src/app/pages/examenes/examenes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-examenes',
+  imports: [],
+  templateUrl: './examenes.html',
+  styleUrl: './examenes.scss',
+})
+export class Examenes {
+
+}
---
+++ src/app/pages/laboratorio/laboratorio.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-laboratorio',
+  imports: [],
+  templateUrl: './laboratorio.html',
+  styleUrl: './laboratorio.scss',
+})
+export class Laboratorio {
+
+}
