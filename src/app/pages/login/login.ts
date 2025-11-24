29d1d3708527d14e9efc76ad56ff90207f193e9a
jai-valencia <jai.valencia@duocuc.cl>
2025-11-23 22:50:42 -0300

    correccion en importacion

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.html',
 })
 export class LoginComponent {
